27d01eecd08caa3790a92e58051d8e048f4395b9
tejasbana <tejasbana@gmail.com>
2021-04-17 01:18:45 +0530

    Add Update & Delete

--- app.py
+++ app.py
@@ -32,5 +32,31 @@ def index():
         tasks = Todo.query.order_by(Todo.data_created).all()
         return render_template("index.html", tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that task"
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task_to_update)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
